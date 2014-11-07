fffc5947ed8cefcd2af6181dfc1f052e9fa76b75
Simon Kim <simon.ty.kim@gmail.com>
2014-11-06 20:11:29 -0800

    Submitting all files

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var app = express();
+var bodyparser = require("body-parser");
+
+app.use(bodyparser.json());
+
+mongoose.connect(process.env.MONGO_URL || "mongodb://localhost/notes_development");
+
+require("./routes/notes_routes")(app);
+
+app.set("port", process.env.PORT || 3000);
+
+app.listen(app.get("port"), function() {
+  console.log("server running on port: " + app.get("port"));
+});
