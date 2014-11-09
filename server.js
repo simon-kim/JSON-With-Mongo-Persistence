c6ddf7509289e79721f69dbea31112d5f43de6e7
Simon Kim <simon.ty.kim@gmail.com>
2014-11-08 18:57:32 -0800

    Update server.js

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ require("./routes/notes_routes")(app);
 app.set("port", process.env.PORT || 3000);
 
 app.listen(app.get("port"), function() {
-  console.log("server running on port: " + app.get("port"));
+  console.log("server running on port: %d" + app.get("port"));
 });
